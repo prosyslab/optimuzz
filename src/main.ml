2c6b922b82c84ecbbb09d09f228de9564788b8da
Timber-min <44857899+Timber-min@users.noreply.github.com>
2023-01-17 13:38:11 +0900

    [LLFuzz] add helper functions for branching

--- src/main.ml
+++ src/main.ml
@@ -69,6 +69,70 @@ let substitute_instr instr opcode =
   Llvm.replace_all_uses_with instr new_instr;
   Llvm.delete_instruction instr
 
+(** [split_block instr] splits the parent block of [instr] into two blocks
+    and links them by unconditional branch.
+    [instr] becomes the first instruction of the latter block.
+    Returns the former block. *)
+let split_block instr =
+  let block = Llvm.instr_parent instr in
+  let new_block = Llvm.insert_block llctx "" block in
+  let builder = Llvm.builder_at_end llctx new_block in
+  Llvm.position_before (Llvm.build_br block builder) builder;
+  let rec aux () =
+    match Llvm.instr_begin block with
+    | Llvm.Before i when i = instr -> ()
+    | Llvm.Before i ->
+        let i_clone = Llvm.instr_clone i in
+        Llvm.insert_into_builder i_clone "" builder;
+        Llvm.replace_all_uses_with i i_clone;
+        Llvm.delete_instruction i;
+        aux ()
+    | Llvm.At_end _ -> failwith "NEVER OCCUR"
+  in
+  aux ();
+  new_block
+
+(** [make_conditional instr] substitutes
+    an unconditional branch instruction [instr]
+    into trivial conditional branch instruction.
+    Returns the conditional branch instruction. *)
+let make_conditional instr =
+  let block = Llvm.instr_parent instr in
+  match instr |> Llvm.get_branch |> Option.get with
+  | `Unconditional target_block ->
+      let fbb = Llvm.append_block llctx "" (Llvm.block_parent block) in
+      Llvm.move_block_after target_block fbb;
+      ignore (Llvm.build_unreachable (Llvm.builder_at_end llctx fbb));
+      Llvm.delete_instruction instr;
+      Llvm.build_cond_br
+        (Llvm.const_int (Llvm.integer_type llctx 1) 1)
+        target_block fbb
+        (Llvm.builder_at_end llctx block)
+  | `Conditional _ -> failwith "Conditional branch already"
+
+(** [make_unconditional instr] substitutes
+    a conditional branch instruction [instr]
+    into unconditional branch instruction targets for true-branch.
+    Returns the unconditional branch instruction. *)
+let make_unconditional instr =
+  match instr |> Llvm.get_branch |> Option.get with
+  | `Conditional (_, target_block, _) ->
+      Llvm.delete_instruction instr;
+      Llvm.build_br target_block
+        (Llvm.builder_at_end llctx (Llvm.instr_parent instr))
+  | `Unconditional _ -> failwith "Unconditional branch already"
+
+(** [negate_condition instr] negates the condition of
+    the conditional branch instruction [instr].
+    Returns [instr] (with its condition negated). *)
+let negate_condition instr =
+  match instr |> Llvm.get_branch |> Option.get with
+  | `Conditional (cond, _, _) ->
+      Llvm.set_condition instr
+        (Llvm.build_not cond "" (Llvm.builder_before llctx instr));
+      instr
+  | `Unconditional _ -> failwith "Unconditional branch"
+
 let mutate llm =
   let llm_clone = Llvm_transform_utils.clone_module llm in
   let f =
