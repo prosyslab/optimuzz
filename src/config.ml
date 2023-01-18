b55621f93310bf7e272981bda5e84d23d4bb47e9
yuntae Kim <yuntae.kim@prosys.kaist.ac.kr>
2023-01-18 11:18:10 +0900

    [LLFuzz] add substitute arithmetic instruction and outline of mutate function

--- src/config.ml
+++ src/config.ml
@@ -1,5 +1,5 @@
 let project_home = ref ""
 let out_dir = ref "llfuzz-out"
-let seed_dir = ref ""
+let seed_dir = ref "seed"
 let alive2_bin = ref ""
 let opts = [ ("-out_dir", Arg.Set_string out_dir, "Output directory") ]
