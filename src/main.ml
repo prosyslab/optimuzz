afcdc6e0bbaca08030caf42ca4fef43769ae582f
Timber-min <44857899+Timber-min@users.noreply.github.com>
2023-01-20 13:49:55 +0900

    [LLFuzz] refactor code
    1. Separate mutation operators in new module mutation
    2. Add class of opcodes
    3. Rename some values

--- src/main.ml
+++ src/main.ml
@@ -3,6 +3,8 @@ module F = Format
 let _ = Random.init 1234
 let llctx = Llvm.create_context ()
 
+module M = Mutation
+
 module SeedPool = struct
   type t = Llvm.llmodule Queue.t
 
@@ -45,13 +47,12 @@ let initialize () =
       Config.seed_dir := x)
     usage
 
-(** [random_change_int operand list] substitutes
-    a binary integer arithmetic operand
-    into another integer value *)
-let random_change_int operand list =
+(** [modify_value v l] slightly modifies value [v]
+    or returns a random element of [l]. *)
+let modify_value v l =
   let inttype = Llvm.i32_type llctx in
   let c =
-    match Llvm.int64_of_const operand with
+    match Llvm.int64_of_const v with
     | Some con -> (
         match Random.int 3 with
         | 0 -> Int64.to_int con * 2
@@ -59,116 +60,19 @@ let random_change_int operand list =
         | _ -> Int64.to_int con + 1)
     | None -> Random.int 200 - 100
   in
-  match list with
+  match l with
   | [] -> Llvm.const_int inttype c
-  | _ -> Utils.random [ Utils.random list; Llvm.const_int inttype c ]
+  | _ -> Utils.list_random [ Utils.list_random l; Llvm.const_int inttype c ]
 
-(** [create_instr instr list] create
-    a binary integer arithmetic operation [instr] with opcode [opcode].
-    It does not use extra keywords such as nsw, nuw, exact, etc. *)
-let create_instr instr list =
-  let opcode = Utils.random_change_op Llvm.Opcode.Alloca in
-  let build_op = Utils.build_op opcode in
-  ignore
-    (build_op
-       (random_change_int (Llvm.operand instr 0) list)
-       (random_change_int (Llvm.operand instr 1) list)
-       ""
-       (Llvm.builder_before llctx instr))
-
-(** [substitute_instr instr opcode] substitutes
-    a binary integer arithmetic operation [instr]
-    into another operation (with opcode [opcode]).
-    It does not use extra keywords such as nsw, nuw, exact, etc. *)
-let substitute_instr instr opcode =
-  (* Add, Sub, Mul, UDiv, SDiv, URem, SRem *)
-  let open Llvm.Opcode in
-  let build_op = Utils.build_op opcode in
-  let new_instr =
-    build_op (Llvm.operand instr 0) (Llvm.operand instr 1) ""
-      (Llvm.builder_before llctx instr)
-  in
-  Llvm.replace_all_uses_with instr new_instr;
-  Llvm.delete_instruction instr
-
-(** [lower_instr instr] lowers
-    the given instruction [instr] one step down within its parent block.
-    Returns its updated predecessor, i.e., its previous successor.
-    If [instr] is the last instruction, does nothing and returns [instr]. *)
-let lower_instr instr =
-  match Llvm.instr_succ instr with
-  | At_end _ -> instr
-  | Before instr_succ ->
-      let instr_succ_clone = Llvm.instr_clone instr_succ in
-      Llvm.insert_into_builder instr_succ_clone ""
-        (Llvm.builder_before llctx instr);
-      Llvm.replace_all_uses_with instr_succ instr_succ_clone;
-      Llvm.set_value_name (Llvm.value_name instr_succ) instr_succ_clone;
-      instr_succ_clone
-
-(** [split_block instr] splits the parent block of [instr] into two blocks
-    and links them by unconditional branch.
-    [instr] becomes the first instruction of the latter block.
-    Returns the former block. *)
-let split_block instr =
-  let block = Llvm.instr_parent instr in
-  let new_block = Llvm.insert_block llctx "" block in
-  let builder = Llvm.builder_at_end llctx new_block in
-  Llvm.position_before (Llvm.build_br block builder) builder;
-  let rec aux () =
-    match Llvm.instr_begin block with
-    | Llvm.Before i when i = instr -> ()
-    | Llvm.Before i ->
-        let i_clone = Llvm.instr_clone i in
-        Llvm.insert_into_builder i_clone "" builder;
-        Llvm.replace_all_uses_with i i_clone;
-        Llvm.delete_instruction i;
-        aux ()
-    | Llvm.At_end _ -> failwith "NEVER OCCUR"
-  in
-  aux ();
-  new_block
-
-(** [make_conditional instr] substitutes
-    an unconditional branch instruction [instr]
-    into trivial conditional branch instruction.
-    Returns the conditional branch instruction. *)
-let make_conditional instr =
-  let block = Llvm.instr_parent instr in
-  match instr |> Llvm.get_branch |> Option.get with
-  | `Unconditional target_block ->
-      let fbb = Llvm.append_block llctx "" (Llvm.block_parent block) in
-      Llvm.move_block_after target_block fbb;
-      ignore (Llvm.build_unreachable (Llvm.builder_at_end llctx fbb));
-      Llvm.delete_instruction instr;
-      Llvm.build_cond_br
-        (Llvm.const_int (Llvm.integer_type llctx 1) 1)
-        target_block fbb
-        (Llvm.builder_at_end llctx block)
-  | `Conditional _ -> failwith "Conditional branch already"
-
-(** [make_unconditional instr] substitutes
-    a conditional branch instruction [instr]
-    into unconditional branch instruction targets for true-branch.
-    Returns the unconditional branch instruction. *)
-let make_unconditional instr =
-  match instr |> Llvm.get_branch |> Option.get with
-  | `Conditional (_, target_block, _) ->
-      Llvm.delete_instruction instr;
-      Llvm.build_br target_block
-        (Llvm.builder_at_end llctx (Llvm.instr_parent instr))
-  | `Unconditional _ -> failwith "Unconditional branch already"
-
-(** [negate_condition instr] negates the condition of
-    the conditional branch instruction [instr].
-    Returns [instr] (with its condition negated). *)
-let negate_condition instr =
-  match instr |> Llvm.get_branch |> Option.get with
-  | `Conditional (cond, _, _) ->
-      Llvm.set_condition instr
-        (Llvm.build_not cond "" (Llvm.builder_before llctx instr));
-      instr
-  | `Unconditional _ -> failwith "Unconditional branch"
+(** [create_random_instr instr l] create
+    a random binary integer arithmetic operation [instr]
+    with a list of possible operands [l].
+    Returns the new instruction. *)
+let create_random_instr instr l =
+  let opcode = Utils.random_opcode_except None in
+  M.create_arith_instr llctx instr opcode
+    (modify_value (Llvm.operand instr 0) l)
+    (modify_value (Llvm.operand instr 1) l)
 
 let mutate llm =
   let llm_clone = Llvm_transform_utils.clone_module llm in
@@ -178,34 +82,34 @@ let mutate llm =
     | Before f -> f
     | At_end _ -> failwith "No function defined"
   in
-
-  let list =
+  let arith_instr_list =
     Utils.fold_left_all_instr
-      (fun l g -> if Utils.is_arith_op (Llvm.instr_opcode g) then g :: l else l)
+      (fun a i ->
+        if
+          i |> Llvm.instr_opcode |> Utils.OpcodeClass.classify
+          = Utils.OpcodeClass.ARITH
+        then i :: a
+        else a)
       [] f
   in
-
-  (match list with
-  | [] ->
-      let mutate_fun =
-        Utils.random [ (fun _ -> create_instr (Utils.get_return_instr f) []) ]
-      in
-      mutate_fun None
+  (match arith_instr_list with
+  | [] -> ignore (create_random_instr (Utils.get_return_instr f) [])
   | _ ->
-      let i = Utils.random list in
-      let arg_list = Utils.get_arguments_from_function f i in
+      let i = Utils.list_random arith_instr_list in
+      let assign_list = Utils.get_assignments_before i in
       let mutate_fun =
-        Utils.random
+        Utils.list_random (* selecting the method of mutation *)
           [
             (fun i ->
-              Llvm.set_operand i 0
-                (random_change_int (Llvm.operand i 0) arg_list));
+              Llvm.set_operand i 0 (modify_value (Llvm.operand i 0) assign_list));
             (fun i ->
-              Llvm.set_operand i 1
-                (random_change_int (Llvm.operand i 1) arg_list));
+              Llvm.set_operand i 1 (modify_value (Llvm.operand i 1) assign_list));
             (fun i ->
-              substitute_instr i (Utils.random_change_op (Llvm.instr_opcode i)));
-            (fun i -> create_instr i arg_list);
+              i |> Llvm.instr_opcode |> Option.some
+              |> Utils.random_opcode_except
+              |> M.substitute_arith_instr llctx i
+              |> ignore);
+            (fun i -> ignore (create_random_instr i assign_list));
           ]
       in
       mutate_fun i);
--- src/utils.ml
+++ src/utils.ml
@@ -1,30 +1,65 @@
-(** [is_arith_op] check opcode is arithmetic *)
-let is_arith_op = function
-  | Llvm.Opcode.Add | Sub | Mul | UDiv | SDiv | URem | SRem -> true
-  | _ -> false
+exception Out_of_integer_domain
 
-(** [get_list_index l v] get index of value(v) in list(l) *)
+module OpcodeClass = struct
+  type opcls_t = TER | ARITH | LOGIC | MEM | CAST | CMP | PHI | OTHER
+
+  let ter_list = []
+  let arith_list = [ Llvm.Opcode.Add; Sub; Mul; UDiv; SDiv; URem; SRem ]
+  let logic_list = []
+  let mem_list = []
+  let cast_list = []
+  let phi_list = []
+  let other_list = []
+
+  let classify = function
+    | Llvm.Opcode.Ret | Br -> TER
+    | Add | Sub | Mul | UDiv | SDiv | URem | SRem -> ARITH
+    | _ when false -> LOGIC (* Shl | LShr | AShr | And | Or | Xor *)
+    | _ when false -> MEM (* Alloca | Load | Store *)
+    | _ when false -> CAST
+    | ICmp -> CMP
+    | PHI -> PHI
+    | FAdd | FSub | FMul | FDiv | FRem | FCmp -> raise Out_of_integer_domain
+    | _ -> OTHER
+
+  let is_assignment opcode =
+    match classify opcode with
+    | ARITH | LOGIC | CMP | PHI -> true
+    | _ -> (
+        (* temporaily except alloca *)
+        match opcode with
+        | Invoke | Invalid2 | Load | GetElementPtr | Trunc | ZExt | SExt
+        | FPToUI | FPToSI | UIToFP | SIToFP | FPTrunc | FPExt | PtrToInt
+        | IntToPtr | BitCast | Select | UserOp1 | UserOp2 | VAArg
+        | ExtractElement | InsertElement | ShuffleVector | ExtractValue
+        | InsertValue | Call | LandingPad ->
+            true
+        | _ -> false)
+
+  (** [build_op opcode] get appropriate build function according to [opcode]. *)
+  let build_arith_op = function
+    | Llvm.Opcode.Add -> Llvm.build_add
+    | Sub -> Llvm.build_sub
+    | Mul -> Llvm.build_mul
+    | UDiv -> Llvm.build_udiv
+    | SDiv -> Llvm.build_sdiv
+    | URem -> Llvm.build_urem
+    | SRem -> Llvm.build_srem
+    | _ -> failwith "Unsupported"
+end
+
+(** [get_list_index l v] returns the index of value [v] in list [l].
+    Raises [Not_found] if does not exist. *)
 let get_list_index l v =
   let rec aux l i =
     match l with
-    | [] -> -1
-    | h :: t -> if compare h v = 0 then i else aux t (i + 1)
+    | [] -> raise Not_found
+    | h :: t -> if h = v then i else aux t (i + 1)
   in
   aux l 0
 
-(** [is_assignment] check opcode of instruction is assignment *)
-let is_assignment = function
-  | Llvm.Opcode.Invoke | Invalid2 | Add | FAdd | Sub | FSub | Mul | FMul | UDiv
-  | SDiv | FDiv | URem | SRem | FRem | Shl | LShr | AShr | And | Or | Xor | Load
-  | GetElementPtr | Trunc | ZExt | SExt | FPToUI | FPToSI | UIToFP | SIToFP
-  | FPTrunc | FPExt | PtrToInt | IntToPtr | BitCast | ICmp | FCmp | PHI | Select
-  | UserOp1 | UserOp2 | VAArg | ExtractElement | InsertElement | ShuffleVector
-  | ExtractValue | InsertValue | Call | LandingPad ->
-      true
-  | _ -> false
-
-(** [fold_left_all_instr f a m] get all instructions from m(llvalue)
-    to a(default value) by f(function) *)
+(** [fold_left_all_instr f a m] returns [f (... f (f (f a i1) i2) i3 ...) iN],
+    where [i1 ... iN] are the instructions in function [m]. *)
 let fold_left_all_instr f a m =
   if Llvm.is_declaration m then a
   else
@@ -32,48 +67,40 @@ let fold_left_all_instr f a m =
       (fun a blk -> Llvm.fold_left_instrs (fun a instr -> f a instr) a blk)
       a m
 
-(** [get_return_instr f] get return instruction from f(llvalue) *)
+(** [get_return_instr f] returns ret instruction from [f]. *)
 let get_return_instr f =
-  let list = fold_left_all_instr (fun l g -> g :: l) [] f in
   List.find
-    (fun l ->
-      l |> Llvm.string_of_llvalue |> String.split_on_char ' '
-      |> List.exists (fun g -> g = "ret"))
-    list
+    (fun instr -> Llvm.instr_opcode instr = Llvm.Opcode.Ret)
+    (fold_left_all_instr (fun l g -> g :: l) [] f)
 
-(** [get_arguments_from_function m i] get all arguments which is declared
-    before i(instr) in m(llvalue) *)
-let get_arguments_from_function m i =
+(** [get_assignments_before i] returns
+    a list of all assignments before [i] in its ancestral function. *)
+let get_assignments_before i =
   let list =
     fold_left_all_instr
-      (fun l g -> if is_assignment (Llvm.instr_opcode g) then g :: l else l)
-      [] m
+      (fun l g ->
+        if g |> Llvm.instr_opcode |> OpcodeClass.is_assignment then g :: l
+        else l)
+      []
+      (i |> Llvm.instr_parent |> Llvm.block_parent)
   in
-  let rec aux l i m =
-    match l with [] -> m | h :: t -> if i = h then m else aux t i (h :: m)
+  let rec aux l i accu =
+    match l with
+    | [] -> accu
+    | h :: t -> if i = h then accu else aux t i (h :: accu)
   in
   aux (List.rev list) i []
 
-(** [random list] get random value from list *)
-let random list =
-  match list with
+(** [list_random l] returns a random element from a list [l]. *)
+let list_random l =
+  match l with
   | [] -> failwith "Empty list"
-  | _ -> List.nth list (Random.int (List.length list))
+  | _ -> List.nth l (Random.int (List.length l))
 
-(** [random_change_op op] get operator different with recent operator(op) *)
-let random_change_op op =
-  let oplist = [ Llvm.Opcode.Add; Sub; Mul; UDiv; SDiv; URem; SRem ] in
-  List.filter (fun x -> compare x op <> 0) oplist |> random
-
-(** [build_op opcode] get appropriate build function according to opcode*)
-let build_op opcode =
-  let open Llvm.Opcode in
-  match opcode with
-  | Add -> Llvm.build_add
-  | Sub -> Llvm.build_sub
-  | Mul -> Llvm.build_mul
-  | UDiv -> Llvm.build_udiv
-  | SDiv -> Llvm.build_sdiv
-  | URem -> Llvm.build_urem
-  | SRem -> Llvm.build_srem
-  | _ -> failwith "Unsupported"
+(** [random_opcode_except opcode] returns an opcode other than [opcode]. *)
+let random_opcode_except opcode =
+  let opcode_list = [ Llvm.Opcode.Add; Sub; Mul; UDiv; SDiv; URem; SRem ] in
+  list_random
+    (match opcode with
+    | Some opcode_ex -> List.filter (fun x -> x <> opcode_ex) opcode_list
+    | None -> opcode_list)
