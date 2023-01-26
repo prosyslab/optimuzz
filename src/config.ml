72f80864c89f82ade714eef8e33f253005b1d687
yuntae Kim <yuntae.kim@prosys.kaist.ac.kr>
2023-01-26 12:05:09 +0900

    [LLFuzz] get line coverage of opt and measure interest

--- src/config.ml
+++ src/config.ml
@@ -2,10 +2,10 @@ let project_home = ref ""
 let out_dir = ref "llfuzz-out"
 let seed_dir = ref "seed"
 let alive2_bin = ref "alive2/build/alive-tv"
-let bin = ref "llvm-project/build/bin/llc"
-let gcno = ref "llvm-project/build/tools/llc/CMakeFiles/llc.dir/llc.cpp.gcno"
-let gcda = ref "llvm-project/build/tools/llc/CMakeFiles/llc.dir/llc.cpp.gcda"
-let gcov = ref "llc.cpp.gcov"
+let bin = ref "llvm-project/build/bin/opt"
+let gcno = ref "llvm-project/build/tools/opt/CMakeFiles/opt.dir/opt.cpp.gcno"
+let gcda = ref "llvm-project/build/tools/opt/CMakeFiles/opt.dir/opt.cpp.gcda"
+let gcov = ref "opt.cpp.gcov"
 let no_tv = ref false
 
 let opts =
