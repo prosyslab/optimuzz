f99b4b62b51953792a76a320e4b69c4ac28f5a2f
Timber-min <44857899+Timber-min@users.noreply.github.com>
2023-01-31 10:39:29 +0900

    [Mutation] minor changes
    - modify helper functions
    - modify comments
    - remove unnecessry code
    - rename values

--- src/utils.ml
+++ src/utils.ml
@@ -50,13 +50,16 @@ module OpcodeClass = struct
 
   (** [random_opcode_except opcode] returns an opcode
       of its class other than [opcode] if given,
-      else, it returns a totally random opcode. *)
+      else, it returns a totally random opcode.
+      If [opcode] is the only one in its class, returns [opcode]. *)
   let random_opcode_except opcode =
-    list_random
-      (match opcode with
-      | Some opcode ->
+    match opcode with
+    | Some opcode ->
+        let l =
           List.filter (fun x -> x <> opcode) (opcode |> classify |> oplist_of)
-      | None -> total_list)
+        in
+        if l <> [] then list_random l else opcode
+    | None -> list_random total_list
 
   let is_assignment opcode =
     match classify opcode with
@@ -92,8 +95,7 @@ module OpcodeClass = struct
     | _ -> raise Unsupported
 
   (* each MEM instruction has different operands *)
-  let build_mem = function
-    | _ -> failwith "Handle MEM instructions individually."
+  let build_mem _ = raise Unsupported
 
   let build_cast = function
     | Llvm.Opcode.Trunc -> Llvm.build_trunc
@@ -132,31 +134,22 @@ let get_return_instr f =
     (fun instr -> Llvm.instr_opcode instr = Llvm.Opcode.Ret)
     (fold_left_all_instr (fun l g -> g :: l) [] f)
 
-(** [get_type_before i t] returns
-    a list of all value which have type[t] before [i] in its ancestral function. *)
-let get_type_before i t =
+(** [get_assignments_before i] returns
+    a list of all assignments before [i] in its ancestral function. *)
+let get_assignments_before i =
   let rec aux rev_pos accu =
     match rev_pos with
     | Llvm.At_start _ -> accu
     | After i ->
-        aux (Llvm.instr_pred i) (if Llvm.type_of i = t then i :: accu else accu)
+        aux (Llvm.instr_pred i)
+          (if i |> Llvm.instr_opcode |> OpcodeClass.is_assignment then i :: accu
+          else accu)
   in
   aux (Llvm.instr_pred i) []
 
-(** [get_blocks_after bb] returns
-    a list of all blocks after [bb] in its parent function. *)
-let get_blocks_after bb =
-  let rec aux rev_pos accu =
-    match rev_pos with
-    | Llvm.At_start _ -> failwith "NEVER OCCUR"
-    | After block ->
-        if block = bb then accu else aux (Llvm.block_pred block) (block :: accu)
-  in
-  aux (bb |> Llvm.block_parent |> Llvm.block_end) []
-
-(** [get_alloca_from_function i] get all allocated ptr which is declared
-    before i(instr) in its ancestral function. *)
-let get_alloca_from_function i =
+(** [get_alloca_before i] returns
+    a list of all allocation pointers before [i] in its ancestral function. *)
+let get_alloca_before i =
   let list =
     fold_left_all_instr
       (fun l g ->
@@ -170,6 +163,21 @@ let get_alloca_from_function i =
     | h :: t -> if i = h then accu else aux t i (h :: accu)
   in
   aux (List.rev list) i []
+
+(** [filter_by_type ty vl] returns
+    a filtered version of [vl]; only llvalues with type [ty] remains. *)
+let filter_by_type ty vl = List.filter (fun v -> Llvm.type_of v = ty) vl
+
+(** [get_blocks_after bb] returns
+    a list of all blocks after [bb] in its parent function. *)
+let get_blocks_after bb =
+  let rec aux rev_pos accu =
+    match rev_pos with
+    | Llvm.At_start _ -> failwith "NEVER OCCUR"
+    | After block ->
+        if block = bb then accu else aux (Llvm.block_pred block) (block :: accu)
+  in
+  aux (bb |> Llvm.block_parent |> Llvm.block_end) []
 
 let all_arith_instrs_of f =
   fold_left_all_instr
