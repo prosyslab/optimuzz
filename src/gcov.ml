cfdf766d11bc9422c4cd2b542e603016371f5f89
Kihong Heo <kihong.heo@kaist.ac.kr>
2023-06-08 10:56:45 +0900

    [LLFuzz] clean up

---
+++ src/gcov.ml
@@ -0,0 +1,41 @@
+open Domain
+module F = Format
+
+let run () =
+  Unix.chdir !Config.gcov_dir;
+  let devnull = Unix.openfile "/dev/null" [ Unix.O_WRONLY ] 0o644 in
+  List.iter
+    (fun elem ->
+      let llvm_pid =
+        Unix.create_process "llvm-cov"
+          [| "llvm-cov"; "gcov"; "-b"; "-c"; elem |]
+          Unix.stdin devnull devnull
+      in
+      Unix.waitpid [] llvm_pid |> ignore)
+    !Config.gcda_list;
+  Unix.chdir !Config.workspace
+
+let get_line_coverage chunks =
+  try chunks |> List.hd |> String.trim |> int_of_string with Failure _ -> -1
+
+let get_coverage () =
+  run ();
+  let rec aux fp accu =
+    match input_line fp with
+    | line ->
+        (* each line is the form of [COVERED_TIMES:LINE_NUM:CODE] *)
+        let chunks = String.split_on_char ':' line in
+        if get_line_coverage chunks > 0 then
+          aux fp
+            (LineSet.add
+               (List.nth chunks 1 |> String.trim |> int_of_string)
+               accu)
+        else aux fp accu
+    | exception End_of_file ->
+        close_in fp;
+        accu
+  in
+  List.fold_left
+    (fun accu elem ->
+      LineCoverage.add elem (aux (open_in elem) LineSet.empty) accu)
+    LineCoverage.empty !Config.gcov_list
--- src/main.ml
+++ src/main.ml
@@ -62,46 +62,6 @@ let initialize () =
   Config.init_gcov_list ();
   Random.init !Config.random_seed
 
-let run_gcov () =
-  Unix.chdir !Config.gcov_dir;
-  let devnull = Unix.openfile "/dev/null" [ Unix.O_WRONLY ] 0o644 in
-  List.iter
-    (fun elem ->
-      let llvm_pid =
-        Unix.create_process "llvm-cov"
-          [| "llvm-cov"; "gcov"; "-b"; "-c"; elem |]
-          Unix.stdin devnull devnull
-      in
-      Unix.waitpid [] llvm_pid |> ignore)
-    !Config.gcda_list;
-  Unix.chdir !Config.workspace
-
-let get_coverage () =
-  run_gcov ();
-  let rec aux fp accu =
-    match input_line fp with
-    | line ->
-        (* each line is the form of [COVERED_TIMES:LINE_NUM:CODE] *)
-        let chunks = String.split_on_char ':' line in
-        if
-          (try chunks |> List.hd |> String.trim |> int_of_string
-           with Failure _ -> -1)
-          > 0
-        then
-          aux fp
-            (LineSet.add
-               (List.nth chunks 1 |> String.trim |> int_of_string)
-               accu)
-        else aux fp accu
-    | exception End_of_file ->
-        close_in fp;
-        accu
-  in
-  List.fold_left
-    (fun accu elem ->
-      LineCoverage.add elem (aux (open_in elem) LineSet.empty) accu)
-    LineCoverage.empty !Config.gcov_list
-
 let run_alive2 filename =
   (* run alive2 *)
   let pid =
@@ -160,17 +120,19 @@ let run llm =
   (* run opt/alive2 and evaluate *)
   if !Config.no_tv then (
     let exit_code =
-      Sys.command (!Config.opt_bin ^ " -S --passes=instcombine " ^ output_name)
+      Sys.command
+        (!Config.opt_bin ^ " -S --passes=instcombine -o /dev/null "
+       ^ output_name)
     in
     let crashed = exit_code <> 0 in
     if crashed then
       Llvm.print_module (Filename.concat !Config.crash_dir (ll_of_count ())) llm;
-    ((if crashed then CRASH else VALID), get_coverage ()))
+    ((if crashed then CRASH else VALID), Gcov.get_coverage ()))
   else
     let alive2_result = run_alive2 output_name in
     if alive2_result <> VALID then
       Llvm.print_module (Filename.concat !Config.crash_dir (ll_of_count ())) llm;
-    (alive2_result, get_coverage ())
+    (alive2_result, Gcov.get_coverage ())
 
 let rec fuzz pool cov =
   let seed, pool_popped = SeedPool.pop pool in
