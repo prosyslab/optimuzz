804a3597aa3e061c7f08ff213e4323c9c08f5bc5
Timber-min <44857899+Timber-min@users.noreply.github.com>
2023-06-27 10:32:18 +0900

    [Pattern] add generate_simplest

--- src/pattern.ml
+++ src/pattern.ml
@@ -1,7 +1,7 @@
-type tree =
+type pat_t =
   | Const of int
   | Variable of string
-  | Operator of Llvm.Opcode.t * tree list
+  | Operator of Llvm.Opcode.t * pat_t list
 
 let rec string_of_tree = function
   | Const i -> "Const(" ^ string_of_int i ^ ")"
@@ -16,9 +16,11 @@ let to_node name arg =
   match name with
   | "m_One" -> Const 1
   | "m_Zero" -> Const 0
-  | "m_Value" -> Variable arg
+  | "m_Value" | "m_Specific" | "m_APInt" -> Variable arg
   | _ -> raise (Invalid_argument name)
 
+(* TODO: Handle aliases e.g., m_Not *)
+(* TODO: Handle commutative versions e.g., m_c_Add *)
 let to_opcode name =
   Utils.opcode_of_string (String.sub name 2 (String.length name - 2))
 
@@ -62,3 +64,54 @@ let parse pat_filename =
     else to_node name sp
   in
   (name, build pat)
+
+let generate_simplest pat : Llvm.llmodule =
+  let llctx = Llvm.create_context () in
+  let llm = Llvm.create_module llctx "test" in
+  let i32 = Llvm.i32_type llctx in
+
+  (* find all variables in the pattern *)
+  let module ParamMap = Map.Make (String) in
+  let rec number_vars m p =
+    match p with
+    | Const _ -> m
+    | Variable v ->
+        if ParamMap.mem v m then m else ParamMap.add v (ParamMap.cardinal m) m
+    | Operator (_, sps) -> List.fold_left number_vars m sps
+  in
+  let param_idx_map = number_vars ParamMap.empty pat in
+
+  let f =
+    Llvm.define_function "f"
+      (Llvm.function_type i32
+         (Array.make (ParamMap.cardinal param_idx_map) i32))
+      llm
+  in
+  let param_llv_map =
+    ParamMap.mapi
+      (fun param idx ->
+        let llv = Llvm.param f idx in
+        Llvm.set_value_name param llv;
+        llv)
+      param_idx_map
+  in
+  let builder = Llvm.builder_at_end llctx (Llvm.entry_block f) in
+
+  (* Postorder traversal of the tree. Involves parameter introduction. *)
+  let rec aux (p : pat_t) : Llvm.llvalue =
+    match p with
+    | Const i -> Llvm.const_int i32 i
+    | Variable v -> ParamMap.find v param_llv_map
+    | Operator (opcode, sps) -> (
+        let operands = List.map aux sps in
+        (* TODO: handle non-binary operators *)
+        let o0 = List.hd operands in
+        let o1 = operands |> List.tl |> List.hd in
+        match Utils.OpcodeClass.classify opcode with
+        | ARITH -> Utils.OpcodeClass.build_arith opcode o0 o1 builder
+        | LOGIC -> Utils.OpcodeClass.build_logic opcode o0 o1 builder
+        | _ -> failwith "Not implemented")
+  in
+  Llvm.build_ret (aux pat) builder |> ignore;
+  Llvm.print_module "test.ll" llm;
+  failwith "2"
