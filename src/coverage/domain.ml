6ca9c12c4c226e6995d58c08d52f851eb27d0af0
Bongjun Jang <bongjun.jang@kaist.ac.kr>
2024-01-12 15:00:27 +0900

    [Coverage] remove duplicate domain definition

--- src/coverage/domain.ml
+++ src/coverage/domain.ml
@@ -9,30 +9,30 @@ end
 module Id = Int
 
 (* set of id *)
-module Group = Set.Make (Id)
+module CovSet = Set.Make (Int)
 
 (* map from location to group  *)
 module CovMap = struct
   module LocMap = Map.Make (Loc)
 
-  type t = Group.t LocMap.t
+  type t = CovSet.t LocMap.t
 
   let empty : t = LocMap.empty
-  let add : Loc.t -> Group.t -> t -> t = LocMap.add
+  let add : Loc.t -> CovSet.t -> t -> t = LocMap.add
 
   let mem loc id cov_map =
     match LocMap.find_opt loc cov_map with
-    | Some group -> Group.mem id group
+    | Some group -> CovSet.mem id group
     | None -> false
 
-  let update : Loc.t -> (Group.t option -> Group.t option) -> t -> t =
+  let update : Loc.t -> (CovSet.t option -> CovSet.t option) -> t -> t =
     LocMap.update
 
   (** [join x y] adds all coverage in [y] into [x]. *)
   let join =
     LocMap.merge (fun _ d_x d_y ->
         match (d_x, d_y) with
-        | Some d_x, Some d_y -> Some (Group.union d_x d_y)
+        | Some d_x, Some d_y -> Some (CovSet.union d_x d_y)
         | Some d, None | None, Some d -> Some d
         | None, None -> None)
 
@@ -41,12 +41,10 @@ module CovMap = struct
     LocMap.for_all
       (fun k d_x ->
         match LocMap.find_opt k y with
-        | Some d_y -> Group.subset d_x d_y
+        | Some d_y -> CovSet.subset d_x d_y
         | None -> false)
       x
 
   (** [cardinal x] returns the sum of cardinals of all bindings in [x]. *)
-  let cardinal x = LocMap.fold (fun _ d accu -> accu + Group.cardinal d) x 0
+  let cardinal x = LocMap.fold (fun _ d accu -> accu + CovSet.cardinal d) x 0
 end
-
-module CovSet = Set.Make (Int)
