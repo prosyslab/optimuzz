338796453023eca66b9b8dbbf70e851f6e5adf18
Bongjun <bongjun.jang@kaist.ac.kr>
2024-02-09 13:34:38 +0900

    [Misc] refactor main

--- src/main.ml
+++ src/main.ml
@@ -24,6 +24,27 @@ let initialize () =
 
   ()
 
+let do_pattern_only llset () =
+  let name, pat = !Config.pattern_path |> Pattern.Parser.run in
+  let all_instances = Pattern.Instantiation.run name pat in
+  List.iter
+    (fun llm ->
+      match ALlvm.LLModuleSet.get_new_name llset llm with
+      | None -> ()
+      | Some filename -> ALlvm.save_ll !Config.out_dir filename llm)
+    all_instances;
+  exit 0
+
+let measure_coverage_only ~passes () =
+  let open Oracle in
+  let res = Optimizer.run ~passes !Config.cov_tgt_path in
+  passes |> List.iter (fun pass -> F.printf "Pass: %s@." pass);
+  (match res with
+  | Optimizer.VALID cov ->
+      F.printf "Total coverage: %d@." (CD.Coverage.cardinal cov)
+  | _ -> ());
+  exit 0
+
 let main () =
   let open Oracle in
   Printexc.record_backtrace true;
@@ -33,29 +54,11 @@ let main () =
   let llset = ALlvm.LLModuleSet.create 4096 in
 
   (* pattern *)
-  if !Config.pattern_path <> "" then (
-    let name, pat = !Config.pattern_path |> Pattern.Parser.run in
-    let all_instances = Pattern.Instantiation.run name pat in
-    List.iter
-      (fun llm ->
-        match ALlvm.LLModuleSet.get_new_name llset llm with
-        | None -> ()
-        | Some filename -> ALlvm.save_ll !Config.out_dir filename llm)
-      all_instances;
-    exit 0);
+  if !Config.pattern_path <> "" then do_pattern_only llset ();
 
   (* measure coverage *)
-  if !Config.cov_tgt_path <> "" then (
-    let res =
-      Optimizer.run
-        ~passes:[ "globaldce"; "simplifycfg"; "instsimplify"; "instcombine" ]
-        !Config.cov_tgt_path
-    in
-    (match res with
-    | Optimizer.VALID cov ->
-        F.printf "Total coverage: %d@." (CD.Coverage.cardinal cov)
-    | _ -> ());
-    exit 0);
+  if !Config.cov_tgt_path <> "" then
+    measure_coverage_only ~passes:[ "instcombine" ] ();
 
   (* fuzzing *)
   let seed_pool = SeedPool.make llctx llset in
