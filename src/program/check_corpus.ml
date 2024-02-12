a0891a89a6a0307bbe6cd02acbe3da2d6f632e31
Bongjun <bongjun.jang@kaist.ac.kr>
2024-02-12 17:42:23 +0900

    [Program] add progress bar

--- src/program/check_corpus.ml
+++ src/program/check_corpus.ml
@@ -1,4 +1,5 @@
 open Oracle
+open Util
 
 let collect_module_files ?predicate dir =
   let predicate = match predicate with Some f -> f | None -> fun _ -> true in
@@ -31,9 +32,29 @@ let time f x =
 
 let args = ref []
 let ntasks = ref 12
+let re = ref None
 
 let speclist =
-  [ ("-ntasks", Arg.Int (fun n -> ntasks := n), "the degree of parallelism") ]
+  [
+    ("-ntasks", Arg.Int (fun n -> ntasks := n), "the degree of parallelism");
+    ( "-re",
+      Arg.String (fun s -> re := Some (Str.regexp s)),
+      "regular expression to filter files" );
+  ]
+
+let bar ~total =
+  let open Progress.Line in
+  list [ bar total; count_to total ]
+
+module C = Domainslib.Chan
+
+let report_worker reporter (queue : int C.t) () =
+  let rec iter () =
+    let _ = C.recv queue in
+    reporter 1;
+    iter ()
+  in
+  iter () |> ignore
 
 let _ =
   let open Domainslib in
@@ -43,14 +64,34 @@ let _ =
 
   let dir = !args |> List.hd in
 
-  let llfiles = collect_module_files dir in
+  let llfiles =
+    match !re with
+    | None -> collect_module_files dir
+    | Some re ->
+        let finder filename =
+          let content = AUtil.readlines filename in
+          List.exists (fun line -> Str.string_match re line 0) content
+        in
+        collect_module_files ~predicate:finder dir
+  in
+
   Format.printf "Found %d modules@." (Array.length llfiles);
 
   let pool = Task.setup_pool ~num_domains:!ntasks () in
 
-  Task.parallel_for pool ~start:0 ~finish:(Array.length llfiles) ~body:(fun i ->
-      let llfile = llfiles.(i) in
-      let verify = check_transformation llfile in
-      if verify then ()
-      else Format.printf "alive-tv reports wrong transformation: %s@." llfile);
+  let report_queue = C.make_unbounded () in
+  Progress.with_reporter
+    (bar ~total:(Array.length llfiles))
+    (fun reporter ->
+      let report_domain = Domain.spawn (report_worker reporter report_queue) in
+
+      Task.parallel_for pool ~start:0 ~finish:(Array.length llfiles)
+        ~body:(fun i ->
+          let llfile = llfiles.(i) in
+          let verify = check_transformation llfile in
+          C.send report_queue 1;
+          if not verify then
+            Format.printf "alive-tv reports wrong transformation: %s@." llfile);
+
+      Domain.join report_domain);
   ()
