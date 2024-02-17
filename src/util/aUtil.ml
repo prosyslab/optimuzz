743a473b4c5d42085bf69b3c34fc2e8bfffa19c3
Bongjun <bongjun.jang@kaist.ac.kr>
2024-02-17 20:58:54 +0900

    [Logging] remove old logging functions

--- src/util/aUtil.ml
+++ src/util/aUtil.ml
@@ -5,8 +5,6 @@ let timestamp = "timestamp.txt"
 let start_time = ref 0
 let recent_time = ref 0
 let timestamp_fp = open_out timestamp
-let log_path = "fuzzing.log"
-let log_formatter = log_path |> open_out |> Format.formatter_of_out_channel
 
 let get_current_time () =
   let tm = Unix.gettimeofday () |> Unix.localtime in
@@ -67,8 +65,6 @@ let readlines filename =
 
 let clean filename =
   cmd [ "rm"; filename; "> /dev/null"; "2> /dev/null" ] |> ignore
-
-let log fstr = Format.fprintf log_formatter fstr
 
 module PrioQueue = struct
   type priority = int
