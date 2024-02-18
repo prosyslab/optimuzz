e4356f199f882ad55b849044a960ec0fbbcddfd6
Bongjun <bongjun.jang@kaist.ac.kr>
2024-02-18 03:43:54 +0000

    [Script] fix build script

--- Alive2/build.sh
+++ Alive2/build.sh
@@ -1,3 +1,5 @@
+git submodule update --init llvm-project alive2
+
 mkdir -p llvm-project/build
 cd llvm-project/build
 cmake -GNinja -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DLLVM_ENABLE_RTTI=ON -DLLVM_ENABLE_EH=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_PROJECTS="llvm;clang" ../llvm
