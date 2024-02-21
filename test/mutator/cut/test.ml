0bdb08dcaf3b0d4cee44b97cd8c69f618128a33d
Bongjun <bongjun.jang@kaist.ac.kr>
2024-02-21 06:47:07 +0000

    [Test] update test drivers conforming to the new sig

--- test/mutator/cut/test.ml
+++ test/mutator/cut/test.ml
@@ -3,14 +3,15 @@ open Util
 let main () =
   let llctx = ALlvm.create_context () in
   let llm = ALlvm.read_ll llctx Sys.argv.(1) in
-  let func = ALlvm.choose_function llm in
-  let all_instrs =
-    ALlvm.fold_left_all_instr (fun acc instr -> instr :: acc) [] func
-  in
-  let n = int_of_string Sys.argv.(2) in
-  let tgt = List.nth all_instrs n in
 
-  Fuzz.Mutator.cut_below llctx tgt |> ignore;
+  Logger.from_file "debug.log";
+  Logger.set_level Logger.DEBUG;
+
+  Logger.debug "before:\n%s" (ALlvm.string_of_llmodule llm);
+  (match Fuzz.Mutator.cut_below llctx llm with
+  | None -> Logger.debug "no change"
+  | Some llm' -> Logger.debug "after:\n%s" (ALlvm.string_of_llmodule llm'));
+
   ()
 
 let _ = main ()
