4b4b92ddbf148abf5803129e82f8ff20ede0c770
Bongjun <bongjun.jang@kaist.ac.kr>
2024-03-25 08:13:20 +0000

    [Program] create `clean` executable for testing preprocessing

---
+++ src/program/clean.ml
@@ -0,0 +1,23 @@
+open Seedcorpus
+open Util
+
+let main input_file =
+  let llctx = ALlvm.create_context () in
+  Format.printf "input file: %s@." input_file;
+  let llm = ALlvm.read_ll llctx input_file in
+  Format.printf "input module: %s@." (ALlvm.string_of_llmodule llm);
+  Format.printf "refactored cleaning@.";
+  match Prep.clean_llm llctx true llm with
+  | None ->
+      Format.printf "clean failed@.";
+      ()
+  | Some llm ->
+      Format.printf "clean succeeded@.";
+      Format.printf "%s@." (ALlvm.string_of_llmodule llm);
+      ()
+
+let _ =
+  let input = Sys.argv.(1) in
+  if Sys.is_directory input then
+    Sys.readdir input |> Array.map (Filename.concat input) |> Array.iter main
+  else main Sys.argv.(1)
