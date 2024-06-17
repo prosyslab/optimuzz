f4a8e30925cfb42652d4f4c1afc7a97774e802b6
Bongjun <bongjun.jang@kaist.ac.kr>
2024-06-17 04:58:23 +0000

    [Program] update as type changes

--- src/program/clean.ml
+++ src/program/clean.ml
@@ -1,22 +1,21 @@
 open Seedcorpus
 open Util
 
-let main input_file =
+let main seed_dir =
   let llctx = ALlvm.create_context () in
-  Format.printf "input file: %s@." input_file;
-  let llm = ALlvm.read_ll llctx input_file |> Result.get_ok in
-  Format.printf "input module: %s@." (ALlvm.string_of_llmodule llm);
-  match Prep.clean_llm llctx true llm with
-  | None ->
-      Format.printf "clean failed@.";
-      ()
-  | Some llm ->
-      Format.printf "clean succeeded@.";
-      Format.printf "%s@." (ALlvm.string_of_llmodule llm);
-      ()
+  Seedpool.collect_cleaned_seeds llctx seed_dir
 
 let _ =
-  let input = Sys.argv.(1) in
-  if Sys.is_directory input then
-    Sys.readdir input |> Array.map (Filename.concat input) |> Array.iter main
-  else main Sys.argv.(1)
+  if Sys.argv |> Array.length <> 3 then
+    Format.eprintf "Usage: %s <seed_dir> <out_dir>@." Sys.argv.(0)
+  else
+    let seed_dir = Sys.argv.(1) in
+    let out_dir = Sys.argv.(2) in
+    Logger.from_file "cleaned_seeds.log";
+    Config.opt_bin := "./opt";
+    let cleaned_seeds = main seed_dir in
+    cleaned_seeds
+    |> List.iter (fun llm ->
+           let cache_dir = Filename.concat (Sys.getcwd ()) out_dir in
+           let filename = ALlvm.hash_llm llm |> Format.sprintf "id:%010d.ll" in
+           ALlvm.save_ll cache_dir filename llm |> ignore)
