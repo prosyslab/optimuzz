a21e8b074c4ded753db5e09f8943accbf4066b3c
Bongjun <bongjun.jang@kaist.ac.kr>
2024-10-22 01:22:56 +0000

    [Mutation] configurable focus

--- src/program/llmutate.ml
+++ src/program/llmutate.ml
@@ -3,11 +3,13 @@ module L = Logger
 
 type mode = Rand | Exact of Mutation.Domain.mutation_t
 
-let bound = ref 5 (* number of retries to find a proper mutation *)
+let bound = ref 10 (* number of retries to find a proper mutation *)
 let mode = ref Rand
+let focus = ref true
 
 let opts =
   [
+    ("-no-focus", Arg.Clear focus, "disable focusing on important instructions");
     ("-bound", Arg.Set_int bound, "number of retries to find a proper mutation");
     ( "-mode",
       Arg.String (fun s -> mode := Exact (Mutation.Domain.mutation_of_string s)),
@@ -24,13 +26,17 @@ let save_mutated_instrs filename mutated_instrs =
       List.iter (Printf.fprintf instr "%s\n") mutated_instrs)
 
 let main path save_dir save_name =
+  assert (Sys.file_exists path);
+  assert (Sys.file_exists save_dir);
+  assert (Sys.is_directory save_dir);
+
   let llctx = Llvm.create_context () in
   Config.Interests.set_interesting_types llctx;
   let importants_filename = "importants" in
   match ALlvm.read_ll llctx path with
   | Ok llm -> (
       let importants =
-        if Sys.file_exists importants_filename then
+        if !focus && Sys.file_exists importants_filename then
           In_channel.with_open_text importants_filename In_channel.input_all
           |> String.split_on_char '\n'
         else []
@@ -38,8 +44,7 @@ let main path save_dir save_name =
       print_endline "llm:";
       print_endline (ALlvm.string_of_llmodule llm);
       let mutated_instrs, mutant =
-        (* Mutation.Mutator.run ~times:10 llctx llm importants choice *)
-        Mutation.Mutator.run ~times:10 llctx llm importants choice
+        Mutation.Mutator.run ~times:!bound llctx llm importants choice
       in
 
       match mutant with
@@ -57,18 +62,16 @@ let main path save_dir save_name =
 let _ =
   Printexc.record_backtrace true;
   Random.self_init ();
-  if Array.length Sys.argv < 4 then (
-    Format.eprintf
-      "Usage: llmutate <filename> <save_dir> <save_name> <importants>";
-    exit 0);
 
-  Arg.parse opts (fun _ -> ()) "";
+  let args = ref [] in
+  Arg.parse opts
+    (fun s -> args := s :: !args)
+    "Usage: llmutate <filename> <save_dir> <save_name> <importants>";
+  args := List.rev !args;
+  match !args with
+  | [ filename; save_dir; save_name ] ->
+      L.from_file "llmutate.log";
+      L.set_level L.DEBUG;
 
-  L.from_file "llmutate.log";
-  L.set_level L.DEBUG;
-
-  let filename = Sys.argv.(1) in
-  let save_dir = Sys.argv.(2) in
-  let save_name = Sys.argv.(3) |> Filename.basename in
-  let save_path = main filename save_dir save_name in
-  print_endline save_path
+      main filename save_dir save_name |> print_endline
+  | _ -> failwith "invalid arguments"
