efea6ed469fefa5c512c97598e096e2c8105442b
Bongjun <bongjun.jang@kaist.ac.kr>
2024-10-22 04:20:29 +0000

    [Seed] constant energy for baseline

--- src/seedcorpus/domain.ml
+++ src/seedcorpus/domain.ml
@@ -147,10 +147,12 @@ module CfgSeed = struct
   let edge_cov seed = seed.edge_cov
 
   let get_energy seed =
-    let int_score = Float.to_int seed.score in
-    if seed.covers then 12
-    else if int_score >= 10 then 3
-    else Int.sub 12 int_score
+    if !Config.score = Config.FuzzingMode.Constant then 8
+    else
+      let int_score = Float.to_int seed.score in
+      if seed.covers then 12
+      else if int_score >= 10 then 3
+      else Int.sub 12 int_score
 
   let name ?(parent : int option) seed =
     let hash = ALlvm.hash_llm seed.llm in
