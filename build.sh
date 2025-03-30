a881dc2023f658cc76d255d2df3775b2d9d49def
Bongjun <bongjun.jang@kaist.ac.kr>
2025-03-30 07:15:09 +0000

    [Project] update to LLVM-20

--- build.sh
+++ build.sh
@@ -4,27 +4,30 @@ set -e
 export OPAMYES=1
 
 NCPU="$(getconf _NPROCESSORS_ONLN 2>/dev/null || echo 1)"
-OCAML_VERSION="5.0.0"
-LLFUZZ_OPAM_SWITCH=llfuzz-"$OCAML_VERSION"
+OCAML_VERSION="5.3.0"
+OPTIMUZZ_OPAM_SWITCH=optimuzz-"$OCAML_VERSION"
 opam init --reinit --bare --no-setup
 
 switch_exists=no
 for installed_switch in $(opam switch list --short); do
-  if [[ "$installed_switch" == "$LLFUZZ_OPAM_SWITCH" ]]; then
+  if [[ "$installed_switch" == "$OPTIMUZZ_OPAM_SWITCH" ]]; then
     switch_exists=yes
     break
   fi
 done
 
 if [ "$switch_exists" = "no" ]; then
-  opam switch create $LLFUZZ_OPAM_SWITCH $OCAML_VERSION
+  opam switch create $OPTIMUZZ_OPAM_SWITCH $OCAML_VERSION
 else
-  opam switch $LLFUZZ_OPAM_SWITCH
+  opam switch $OPTIMUZZ_OPAM_SWITCH
 fi
 
-eval $(SHELL=bash opam config env --switch=$LLFUZZ_OPAM_SWITCH)
+eval $(SHELL=bash opam config env --switch=$OPTIMUZZ_OPAM_SWITCH)
 opam pin https://github.com/prosyslab/logger.git
-opam install -j $NCPU llvm.16.0.6+nnp
 opam install -j $NCPU dune logger domainslib progress ocamlgraph
-opam install -j $NCPU ocamlformat.0.26.1 merlin ocp-index ocp-indent ocaml-lsp-server # for development
+opam install -j $NCPU ocamlformat merlin ocp-index ocp-indent ocaml-lsp-server # for development
+scripts/llvm-20.sh
+
 make
+# opam install -j $NCPU llvm.16.0.6+nnp
+# make
